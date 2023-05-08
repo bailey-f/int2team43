a6948761202088d9798ae4d7048a0690ef5228c2
Jake Keast <JRKeast2003@outlook.com>
2023-05-08 15:34:23 +0100

    scaled images, changed preprocessing, added layers

--- main.py
+++ main.py
@@ -1,126 +1,69 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import numpy as np
 from tensorflow import keras
-from keras.utils import np_utils
-from scipy.io import loadmat
+from tensorflow.data.experimental import AUTOTUNE
 
-def create_model(X_train, y_train, X_valid, y_valid):
-    shape = X_train.shape
-    
-    # Creating the model
-    model = tf.keras.Sequential()
-    
-    #   Convolution layer
-    model.add(keras.layers.Conv2D(32, 3, input_shape=shape[1:], padding='same'))
-    #   Activation
-    model.add(keras.layers.Activation('relu'))
-    #   Pooling
-    model.add(keras.layers.MaxPooling2D(2))
+global WIDTH, HEIGHT, CHANNELS, CLASSES
 
-    # Repeat
-    model.add(keras.layers.Conv2D(64, 3, activation='relu', padding='same'))
-    model.add(keras.layers.MaxPooling2D(2))
+WIDTH, HEIGHT, CHANNELS = 128, 128, 3
+
+data_splits, info = tfds.load('oxford_flowers102', split=['test', 'validation', 'train'], with_info=True)
+
+(train, valid, test) = data_splits
+
+CLASSES = info.features['label'].num_classes
+
+# tfds.visualization.show_examples(train, info)
+
+
+def parser(tensor):
+    image = tensor['image']
+    image = tf.image.resize(image, (WIDTH, HEIGHT)) / 255.0
+    label = tensor['label']
+    label = tf.one_hot(tensor['label'], 102)
+    return image, label
+
+def create_model():
+    adam = keras.optimizers.Adam(learning_rate=0.001)
+
+    model = keras.Sequential()
+    # model.add(keras.layers.Input(shape=(WIDTH, HEIGHT) + (CHANNELS,)))
+
+    model.add(keras.layers.Conv2D(126, (3, 3), input_shape=(WIDTH, HEIGHT) + (CHANNELS,), padding='same'))
+    model.add(keras.layers.MaxPooling2D())
+    
+
+    model.add(keras.layers.Conv2D(64, (3, 3), padding='same'))
+    model.add(keras.layers.MaxPooling2D())
+   
+
+    model.add(keras.layers.Conv2D(64, (3, 3), padding='same'))
+    model.add(keras.layers.MaxPooling2D())
+    model.add(keras.layers.Dropout(0.2))
 
     model.add(keras.layers.Flatten())
 
-    # Create densely connected layers
-    model.add(keras.layers.Dense(128, activation='relu'))
-    model.add(keras.layers.Dense(102))
+    model.add(keras.layers.Dense(102, activation='relu'))
+    model.add(keras.layers.Dense(CLASSES, activation='softmax'))
 
-    # Softmax activataion layer to select highest probability
-    model.add(keras.layers.Dense(y_valid.shape[1], activation='softmax'))
-
-    # Compile model
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
-def preprocessing(image_set, labels, augment=False):
-    img = []
-    label = []
-    WIDTH, HEIGHT, CHANNELS = 64, 64, 3
-    PERMS = 10
-    for x in range(len(image_set)):
-        # padding the start of the number with zeroes
-        path = 'jpg/image_' + str(image_set[x]).zfill(5) + '.jpg'
-        
-        # Preprocessing to make image sizes uniform
-        image = tf.io.read_file(path)
-        image = tf.io.decode_jpeg(image)
-        processed_image = tf.image.convert_image_dtype(image, tf.float32)
-        processed_image = tf.image.resize(image, [WIDTH, HEIGHT])
+train = train.shuffle(32).map(parser, num_parallel_calls=AUTOTUNE).batch(65).prefetch(AUTOTUNE)
 
-        # Normalise the colour values
-        processed_image = processed_image / 255.0
-        
-        img.append(processed_image)
-        label.append(labels[image_set[x]-1])
-        
-        if augment:
-            # random flips
-            for y in range(PERMS):
-                seed = (y, 0)
-                aug = tf.image.stateless_random_flip_left_right(processed_image, seed=seed)
-                img.append(aug)
-                label.append(labels[image_set[x]-1])
-        
-            # random jpeg quality
-            for y in range(PERMS):
-                seed = (y, 0)
-                aug = tf.image.stateless_random_jpeg_quality(processed_image, 50, 100, seed=seed)
-                img.append(aug)
-                label.append(labels[image_set[x]-1])
+valid = valid.shuffle(32).map(parser, num_parallel_calls=AUTOTUNE).batch(65).prefetch(AUTOTUNE)
 
-            # random crop
-            for y in range(PERMS):
-                seed = (y, 0)
-                aug = tf.image.stateless_random_crop(processed_image, size=[WIDTH, HEIGHT, CHANNELS], seed=seed)
-                img.append(aug)
-                label.append(labels[image_set[x]-1])
+test = test.shuffle(32).map(parser, num_parallel_calls=AUTOTUNE).batch(65).prefetch(AUTOTUNE)
 
-    img = np.asarray(img, dtype=np.float32)
-    label = np.asarray(label, dtype=np.float32)
-    return img, label
-
-# Load datasplits
-setid = loadmat('setid.mat')
-setid.keys()
-valid_id = setid['valid'][0]
-test_id = setid['tstid'][0]
-train_id = setid['trnid'][0]
-
-# Load labels
-labelfile = loadmat('imagelabels.mat')
-label = labelfile['labels'][0]
-for i in range(len(label)):
-    label[i] = label[i] - 1
-
-# Validation set
-print("Preprocessing validation set")
-X_valid, y_valid = preprocessing(valid_id, label)
-y_valid = np_utils.to_categorical(y_valid)
-print('Validation set preprocessed\n')
-
-# Testing set
-#print("Preprocessing testing set (This may take a minute)")
-#X_test, y_test = preprocessing(test_id, label)
-#y_test = np_utils.to_categorical(y_test)
-#print('Testing set preprocessed\n')
-
-# Training set
-print("Preprocessing training set")
-X_train, y_train = preprocessing(train_id, label, augment=True)
-y_train = np_utils.to_categorical(y_train)
-print('Training set preprocessed\n')
-
-print(len(X_train))
-
-model = create_model(X_train, y_train, X_valid, y_valid)
+model = create_model()
 
 print(model.summary())
 
-np.random.seed(42)
-history = model.fit(X_train, y_train, validation_data=(X_valid, y_valid), epochs=25, batch_size=65)
+model.fit(train, validation_data=valid, epochs=25)
 
-scores = model.evaluate(X_valid, y_valid, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+v_scores = model.evaluate(valid, verbose=0)
+t_scores = model.evaluate(test, verbose=0)
+print("Validation set accuracy: %.2f%%" % (v_scores[1]*100))
+print("Testing set accuracy: %.2f%%" % (t_scores[1]*100))
